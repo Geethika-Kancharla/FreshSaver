d26d6ae769f59bb54dad077408bcfb2a6dd8f323
Geethika-Kancharla <geethikak004@gmail.com>
2024-06-14 12:08:48 +0530

    Details page

--- src/pages/Display.js
+++ src/pages/Display.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useFirebase } from '../context/Firebase';
+import Table from '../components/Table';
 
 const Display = () => {
+    const firebase = useFirebase();
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            const fetchedItems = await firebase.listAllItems();
+            setItems(fetchedItems);
+        };
+        fetchItems();
+    }, [firebase]);
+
+    const handleItemDelete = (deletedItemId) => {
+        setItems(items.filter(item => item.id !== deletedItemId));
+    };
+
     return (
-        <div>Display</div>
-    )
+        <div>
+            <h1>Displaying details here</h1>
+            {items.map((item, index) => (
+                <Table key={index} onItemDelete={handleItemDelete} {...item.data()} />
+            ))}
+        </div>
+    );
 }
 
-export default Display
+export default Display;
